42fae5281d137e69202b50619667ace97a05a9f8
Ishaan-10 <b.ishaan10@gmail.com>
2022-02-26 14:03:38 +0530

    db connect

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const testRouter = require('./routes/test');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const db = mongoose.connection;
+mongoose.connect(process.env.DATABASE_KEY, { useNewUrlParser: true, useUnifiedTopology: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Database Connected")
+});
 
 app.use('/test',testRouter);
 
