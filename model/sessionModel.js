55e9a05712a4447d394e7583782957d299d3f6e0
Ishaan-10 <b.ishaan10@gmail.com>
2022-02-26 15:20:33 +0530

    add habit

--- model/sessionModel.js
+++ model/sessionModel.js
@@ -2,26 +2,29 @@ const mongoose = requrie('mongoose');
 
 const Schema = mongoose.Schema;
 
-const SingleSessionSchema = new Schema ({
-  rating: {
-    type: Number,
-    min: 0,
-    max: 5,
-    default: 2, // default: 2, set in state in Frontend
-    required: true,
-  },
+const SingleSessionSchema = new Schema({
+  sessions: [
+    {
+      rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 2, // default: 2, set in state in Frontend
+        required: true,
+      },
 
-  // for the duration frontend will have hours and minutes field 
-  // function to parse hours into minutes since backend will store minutes.
-  // Add the defaults to the frontend.
-  duration: {
-    type: Number,
-    default: 60,
-    required: true,
-  },
-
-  remarks: String,
-}) 
+      // for the duration frontend will have hours and minutes field 
+      // function to parse hours into minutes since backend will store minutes.
+      // Add the defaults to the frontend.
+      duration: {
+        type: Number,
+        default: 60,
+        required: true,
+      },
+      remarks: String
+    }
+  ]
+})
 
 const singleSession = mongoose.model('singleSession', SingleSessionSchema);
-module.exports=singleSession;
+module.exports = singleSession;
--- routes/habits.js
+++ routes/habits.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const habitsRoute = express.Router();
 const userProfile = require('../model/userProfile');
+const singleSession = require('../model/sessionModel');
 
 habitsRoute.get('/',async (req,res)=>{
     console.log(req.query);
@@ -8,6 +9,17 @@ habitsRoute.get('/',async (req,res)=>{
     const user = await userProfile.find({email}).populate('singleSession');
     console.log(user);
     res.json(user);
+})
+
+habitsRoute.post('/add',(req,res)=>{
+    const {name,uid} = req.body;
+    const user = await userProfile.find({uid});
+    user.habits.push({
+        name:name,
+        streak:0,
+        sessions:new singleSession([]) // Changes needed
+    })
+    user.save();
 })
 
 
